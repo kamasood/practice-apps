3fb545c0d90d72eb639cedd0cb74eddb6ac75440
Kamran Masood <kam1088@gmail.com>
2022-03-23 11:18:06 -0700

    completed initial setup of db.js

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -4,3 +4,22 @@ const mongoose = require("mongoose");
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+// connection
+
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`);
+
+// schema
+
+const glossarySchema = mongoose.Schema({
+  word: String,
+  definition: String
+});
+
+// model
+
+const Glossary = mongoose.model('Glossary', glossarySchema);
+
+// export model
+
+module.exports.Glossary = Glossary;
